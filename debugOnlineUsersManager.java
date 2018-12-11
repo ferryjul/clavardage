1625132d7f3dcf0e7887669d6e1301e5f172d2e7
Julien Ferry <ferry@insat.insa-toulouse.fr>
2018-12-11 17:05:20 +0100

    debug test

--- debugOnlineUsersManager.java
+++ debugOnlineUsersManager.java
@@ -8,10 +8,10 @@ public class debugOnlineUsersManager {
 		Thread networkDiscoveryThread = new Thread(networkDiscovery);
 		networkDiscoveryThread.start();
 
-		System.out.println("Attempt from port " + argv[1] + " to port " + argv[0]);
+/*		System.out.println("Attempt from port " + argv[1] + " to port " + argv[0]);
 		OnlineUsersManager networkDiscovery2 = new OnlineUsersManager(Integer.parseInt(argv[1]), Integer.parseInt(argv[0]));
 		Thread networkDiscoveryThread2 = new Thread(networkDiscovery2);
-		networkDiscoveryThread2.start();
+		networkDiscoveryThread2.start();*/
 	}
 
 }
