bdb8fe900674eac0beb14923e23740e063b2f2ff
M. Genoud Quentin <genoud@insa-07438>
2018-12-13 12:11:24 +0100

    History begin

---
+++ History.java
@@ -0,0 +1,51 @@
+package clavardage;
+
+import java.io.IOException;
+import java.util.Date;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.DataOutputStream;
+import java.io.FileWriter;
+
+
+public class History {
+
+	private String id;
+	private File fileHist;
+	private Date sessionDate;
+	private FileWriter fWrit;
+
+
+	public History(String idDist , String fileName, Date connectDate)
+	{
+		this.sessionDate = connectDate;
+		this.id = idDist;
+		this.fileHist = new File(fileName);
+
+		try{
+			fWrit = new FileWriter(fileHist, true);
+			fWrit.write("--@ Pseudo :" + id + "\n");
+			fWrit.write("Date de la session de conversation :" + sessionDate.toString() + "\n");
+			fWrit.flush();
+		} catch (IOException e){
+			System.err.println("Create FileWRITER FAIL");
+			e.printStackTrace();
+		}
+			
+	}
+
+	public void updateHist(String msg) {
+		try{
+			fWrit.write(msg+"\n");
+			fWrit.flush();
+		} catch (IOException e){
+			System.err.println("update history fail");
+			e.printStackTrace();
+		}
+	}
+
+
+
+	
+
+}
